3ac6cdad554b5058a0deabfa348bc20301142d06
sandy <sandyks.lo27@gmail.com>
2021-10-04 15:47:35 -0700

    tried

--- note-app/src/NewNoteForm.js
+++ note-app/src/NewNoteForm.js
@@ -1,26 +1,38 @@
 import React from "react";
 import {useState, useEffect} from "react";
 
-function NewNoteForm(onAddNote) {
 
-  function handleSubmit(e) {
-      e.preventDefault();
-      fetch("http://localhost:8004/XXXXXXXXXXXXXXXXXXXXX", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          title: title,
-          author: author,
-          content: content,
-          tags: tags
-        }),
-      })
-        .then(r => r.json())
-        .then(newNote => onAddNote(newNote));
-    }
-  
+
+function NewNoteForm(onAddNote) {
+    
+    const [title, setTitle] = useState("")
+    const [author, setAuthor] = useState("")
+    const [content, setContent] = useState("")
+    const [tags, setTags] = useState("")
+
+
+  return (
+      <form>
+          <input type="text" name="title" placeholder="title" />
+
+          <input type="text"name="author" placeholder="title" />
+
+          <textarea type="text"name="content" placeholder="title" rows={10} />
+
+          <input type="text"name="tags" placeholder="title"/>
+
+          <input type="submit" />
+
+      </form>
+
+
+  )
+
+
 }
 
-  export default NewNoteForm;
+
+
+
+
+export default NewNoteForm;
